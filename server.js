db2d5f58a4958b90e78932f39b45820ab23a77ea
gilkconcept <gildaskoudou6@gmail.com>
2025-12-31 16:24:19 +0100

    Fix port for Railway

--- server.js
+++ server.js
@@ -572,7 +572,7 @@ app.use((err, req, res, next) => {
 });
 
 // Démarrer le serveur
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log('\n' + '='.repeat(70));
